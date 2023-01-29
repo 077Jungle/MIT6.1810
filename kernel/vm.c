936cf6fa804d2895b4fdc9078858a6b5f5accf82
077Jungle <544859995@qq.com>
2023-01-30 03:01:21 +0800

    part2

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint_fun(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for(int j = 0; j < level; j++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if(level < 3)
+        vmprint_fun((pagetable_t)child, level+1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_fun(pagetable, 1);
+}
